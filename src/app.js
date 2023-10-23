50d17c228b9c2ebabe14c3d74138f79db75cb2d5
Sara Parra <131387591+sarap13@users.noreply.github.com>
2023-10-23 14:50:32 +0000

    feat random card generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Creamos la variable que cogerá la clase y al cargar la web ejercerá la función.
+  let typeOfCards = document.querySelectorAll(".type-of-card");
+  let randomType = generateRandomType();
+  // For each para que los elementos de la misma clase cambien a la vez y tengan lo mismo.
+  // Para cada caracter se le aplica una clase con un color predeterminado. Si es este char aplicale ésta clase.
+  typeOfCards.forEach(function(typeOfCard) {
+    typeOfCard.innerHTML = randomType;
+    if (randomType === "♥") {
+      typeOfCard.classList.add("heart-class");
+    }
+    if (randomType === "♦") {
+      typeOfCard.classList.add("diamond-class");
+    }
+    if (randomType === "♠") {
+      typeOfCard.classList.add("spade-class");
+    }
+    if (randomType === "♣") {
+      typeOfCard.classList.add("club-class");
+    }
+  });
+
+  // Selecciona el id number y le aplica la función de generar Random number.
+  document.querySelector("#number").innerHTML = generateRandomNumber();
 };
+
+// Función para generar un simbolo Random
+function generateRandomType() {
+  let symbolTypeOfCard = ["♦", "♥", "♠", "♣"];
+  let randomTypeOfCard = Math.floor(Math.random() * symbolTypeOfCard.length);
+  return symbolTypeOfCard[randomTypeOfCard];
+}
+
+// Función para generar un número de carta Random
+function generateRandomNumber() {
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let randomnumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[randomnumbers];
+}
